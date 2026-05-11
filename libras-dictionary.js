68c6b987dbaf0d5161b89884cf7938c3bd6b24b8
Colegio Hercilia Franca <trabalhoshercilia2024@gmail.com>
2026-05-11 07:59:20 -0300

    Adicionando arquivos iniciais do projeto CaprioLibras

---
+++ libras-dictionary.js
@@ -0,0 +1,11 @@
+// libras-dictionary.js
+const librasDict = {
+  "bom dia": "bom-dia",
+  "olá": "ola",
+  "obrigado": "obrigado",
+  // ... centenas de entradas
+};
+
+const regras = {
+  // remover artigos, pronomes, etc.
+};
